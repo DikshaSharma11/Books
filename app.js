81479b004942fbb9180888ee0d08440f7d9710f8
Diksha Sharma <diksha@softobiz.com>
2022-11-23 15:42:05 +0530

    3.40

---
+++ app.js
@@ -0,0 +1,31 @@
+const express =require('express')
+const path = require('path')
+const dotenv =require('dotenv')
+const morgan=require('morgan')
+const exphbs=require('express-handlebars')
+const connectDB=require('./config/db')
+  var hbs=require('hbs')
+dotenv.config({path: '.\config\config.env'})
+
+connectDB()
+
+
+
+const app=express()
+//logging
+if(process.env.NODE_ENV==='development'){
+    app.use(morgan('dev'))
+}
+
+//handlebars
+// app.engine('.hbs', exphbs({defaultLayout:"main",extname: '.hbs'}));
+app.set('view engine', 'hbs')
+
+//static folder
+app.use(express.static(path.join(__dirname,'public')))
+
+//Routes
+app.use('/',require('./routes/index'))
+const PORT=process.env.PORT||3000
+app.listen(PORT,
+console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
